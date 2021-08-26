a7d5c18494ac9ff362a0e58cfdb34c9482201d35
markogit1 <88459391+markogit1@users.noreply.github.com>
2021-08-26 10:15:54 +0200

    Add files via upload

---
+++ destinations.js
@@ -0,0 +1,83 @@
+var meksikodugme = document.getElementById("meksikodugme");
+var stiklirano = document.getElementById("stiklirano");
+
+meksikodugme.onclick = function(){
+    stiklirano.style.visibility = "visible";
+    stiklirano.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano.style.opacity = "0";
+    },2000)
+}
+
+var argentinadugme = document.getElementById("argentinadugme");
+var stiklirano2 = document.getElementById("stiklirano2");
+
+argentinadugme.onclick = function(){
+    stiklirano2.style.visibility = "visible";
+    stiklirano2.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano2.style.opacity = "0";
+    },2000)
+}
+
+var spaindugme = document.getElementById("spaindugme");
+var stiklirano3 = document.getElementById("stiklirano3");
+
+spaindugme.onclick = function(){
+    stiklirano3.style.visibility = "visible";
+    stiklirano3.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano3.style.opacity = "0";
+    },2000)
+}
+
+var afrikadugme = document.getElementById("afrikadugme");
+var stiklirano4 = document.getElementById("stiklirano4");
+
+afrikadugme.onclick = function(){
+    stiklirano4.style.visibility = "visible";
+    stiklirano4.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano4.style.opacity = "0";
+    },2000)
+}
+
+var indiadugme = document.getElementById("indiadugme");
+var stiklirano5 = document.getElementById("stiklirano5");
+
+indiadugme.onclick = function(){
+    stiklirano5.style.visibility = "visible";
+    stiklirano5.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano5.style.opacity = "0";
+    },2000)
+}
+
+var chinadugme = document.getElementById("chinadugme");
+var stiklirano6 = document.getElementById("stiklirano6");
+
+chinadugme.onclick = function(){
+    stiklirano6.style.visibility = "visible";
+    stiklirano6.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano6.style.opacity = "0";
+    },2000)
+}
+
+var australiadugme = document.getElementById("australiadugme");
+var stiklirano7 = document.getElementById("stiklirano7");
+
+australiadugme.onclick = function(){
+    stiklirano7.style.visibility = "visible";
+    stiklirano7.style.opacity = "1";
+
+    setTimeout(function(){
+        stiklirano7.style.opacity = "0";
+    },2000)
+}
